32d9e9b3399d420d5c8de2911c9561543dcdfbc2
KusamaHub <67394582+captainmarshin@users.noreply.github.com>
2022-08-10 05:12:29 +0300

    Web update

---
+++ js/outofstock.js
@@ -0,0 +1,27 @@
+// outofstock.js
+// Delevoper: Маршин Entertainment
+// Used to show error when user try to open item page with outofstock tag
+
+// 2022
+
+$(document).ready(function() {
+
+	console.log("outofstock.js ready")
+
+	// This is selector for all elements with outofstock class
+
+	var itemsofs = document.querySelectorAll('.outofstock');
+
+	for (var i=0; i < itemsofs.length; i += 1) {
+		itemsofs[i].addEventListener("click", ShowOutOfStock);
+	}
+
+	function ShowOutOfStock() {
+		$(".error_outos").show("slideUp", function() {
+				setTimeout(function(){
+					$(".error_outos").hide("slideDown")
+				}, 3000);
+			});
+	}
+
+});
